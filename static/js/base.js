b31f497bfe0d28cdc26d9d38fa803e070161d200
franciskershaw <franciskershaw2004@hotmail.com>
2021-07-26 16:07:45 +0000

    add basic sidebar functionality

--- static/js/base.js
+++ static/js/base.js
@@ -1,1 +1,31 @@
-console.log("HELLO")
+// Functions to open and close the wishlist and basket sidebars
+$(document).ready( () => {
+
+    // Open wishlist sidebar
+    const wishListIcons = document.querySelectorAll('.wishlist-btn');
+    const wishListSideBar = document.querySelector('.wishlist-sidebar');
+    wishListIcons.forEach((wishListIcon) => {
+        wishListIcon.addEventListener('click', () => {
+            wishListSideBar.classList.add('sidebar-opened');
+        })
+    })
+
+    // Open basket
+    const basketIcons = document.querySelectorAll('.basket-btn');
+    const basketSideBar = document.querySelector('.basket-sidebar');
+    basketIcons.forEach((basketIcon) => {
+        basketIcon.addEventListener('click', () => {
+            basketSideBar.classList.add('sidebar-opened');
+        })
+    })
+
+    // Close sidebars
+    const closeButtons = document.querySelectorAll('.close-sidebar');
+    closeButtons.forEach((closeButton) => {
+        closeButton.addEventListener('click', () => {
+            wishListSideBar.classList.remove('sidebar-opened');
+            basketSideBar.classList.remove('sidebar-opened');
+        })
+    })
+
+})
