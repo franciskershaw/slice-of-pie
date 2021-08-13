3d037cade9826ec9a71da45ba4f5d474ddece8e6
franciskershaw <franciskershaw2004@hotmail.com>
2021-08-13 14:26:27 +0000

    add product view, url and templates directory

--- products/views.py
+++ products/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Product
 
 # Create your views here.
+
+
+def all_products(request):
+    """A view to return the all products page"""
+    products = Product.objects.all()
+
+    context = {
+        'products': products,
+    }
+
+    return render(request, 'products/products.html', context)
+
+
+def product_detail(request, product_id):
+    """A view to return a product's detail page"""
+    product = get_object_or_404(Product, pk=product_id)
+
+    context = {
+        'product': product,
+    }
+
+    return render(request, 'products/product_detail.html', context)
