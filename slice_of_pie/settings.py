aaae3daebf5a0eeec94a749f92e2b99c0a6d4d50
franciskershaw <franciskershaw2004@hotmail.com>
2021-08-13 16:10:39 +0000

    amend database allowed_hosts

--- slice_of_pie/settings.py
+++ slice_of_pie/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['slice-of-pie.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['frank-slice-of-pie.herokuapp.com', 'localhost']
 
 
 # Application definition
