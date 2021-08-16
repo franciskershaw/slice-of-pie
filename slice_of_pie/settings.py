70a696b5500bb468767af61cf983d46c6b8cf89f
franciskershaw <franciskershaw2004@hotmail.com>
2021-08-16 10:28:24 +0000

    uncomment out database if/else statement

--- slice_of_pie/settings.py
+++ slice_of_pie/settings.py
@@ -105,21 +105,21 @@ WSGI_APPLICATION = 'slice_of_pie.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# if 'DATABASE_URL' in os.environ:
-#     DATABASES = {
-#         'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-#     }
-# else:
-#     DATABASES = {
-#         'default': {
-#             'ENGINE': 'django.db.backends.sqlite3',
-#             'NAME': BASE_DIR / 'db.sqlite3',
-#         }
-#     }
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
-DATABASES = {
-    'default': dj_database_url.parse('postgres://itgvyrawiwgqve:2aa068bc9085dbfe7bed7e0047f25bc874075e44ff4de5224a8a2cde0bf8894c@ec2-52-211-158-144.eu-west-1.compute.amazonaws.com:5432/d20mfn9egsnnbh')
-}
+# DATABASES = {
+#     'default': dj_database_url.parse('postgres://itgvyrawiwgqve:2aa068bc9085dbfe7bed7e0047f25bc874075e44ff4de5224a8a2cde0bf8894c@ec2-52-211-158-144.eu-west-1.compute.amazonaws.com:5432/d20mfn9egsnnbh')
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
