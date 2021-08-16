31c6ebbe796478be3522a96fe590b5f50b976c5c
franciskershaw <franciskershaw2004@hotmail.com>
2021-08-16 09:44:11 +0000

    add in if/else statement for database config

--- slice_of_pie/settings.py
+++ slice_of_pie/settings.py
@@ -104,12 +104,18 @@ WSGI_APPLICATION = 'slice_of_pie.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.envrion.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
