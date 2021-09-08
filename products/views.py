8165c96f0cb22d922449974f312749141574926e
franciskershaw <franciskershaw2004@hotmail.com>
2021-09-08 20:45:57 +0000

    start filter js and backend logic

--- products/views.py
+++ products/views.py
@@ -8,6 +8,28 @@ def all_products(request):
     """A view to return the all products page"""
 
     products = Product.objects.all()
+    products_array = []
+    products_materials = []
+    products_angles = []
+    products_levels = []
+
+    if request.GET:
+        print(request.GET)
+        print(request.GET.getlist('material'))
+        if 'material' in request.GET:
+            materials = request.GET.getlist('material')
+            products_materials = list(products.filter(material__name__in=materials))
+        if 'angle' in request.GET:
+            angles = request.GET.getlist('angle')
+            products_angles = list(products.filter(material__name__in=angles))
+        if 'levels' in request.GET:
+            levels = request.GET.getlist('levels')
+            products_levels = list(products.filter(material__name__in=levels))
+        products_array = products_materials + products_angles + products_levels
+        print(products_array)
+
+        if products_array:
+            products = products_array
 
     context = {
         'products': products,
