442daa3c8d821b7c8b0dbdaf82ffaf35107f3e1f
franciskershaw <franciskershaw2004@hotmail.com>
2021-09-09 16:40:20 +0000

    add sorting functionality on products.html

--- products/views.py
+++ products/views.py
@@ -14,49 +14,44 @@ def all_products(request):
     products_angles = []
     products_levels = []
 
+    sort = None
+    direction = None
+
     if request.GET:
-        print('-----------------------------')
-        print(f'Full get request: {request.GET}')
-        print('-----------------------------')
+        # Filtering
         if 'material' in request.GET:
-            print(f'Material GET request: {request.GET.getlist("material")}')
             materials = request.GET.getlist('material')
-            print(f'Materials: {materials}')
-            print('-----------------------------')
             products_materials = list(products.filter(material__name__in=materials))
-            print('-----------------------------')
-            print(f'products_materials: {products_materials}')
-            print('-----------------------------')
         if 'angle' in request.GET:
-            print(f'Angle GET request: {request.GET.getlist("angle")}')
             angles = request.GET.getlist('angle')
-            print(f'Angles: {angles}')
-            print('-----------------------------')
             products_angles = list(products.filter(angle__name__in=angles))
-            print(list(products.filter(angle__name__in=angles)))
-            print('-----------------------------')
-            print(f'products_angles: {products_angles}')
-            print('-----------------------------')
         if 'levels' in request.GET:
-            print(f'Levels GET request: {request.GET.getlist("levels")}')
             levels = request.GET.getlist('levels')
-            print(f'Levels: {levels}')
-            print('-----------------------------')
             products_levels = list(products.filter(level__name__in=levels))
-            print('-----------------------------')
-            print(f'products_levels: {products_levels}')
-            print('-----------------------------')
 
         products_array = products_materials + products_angles + products_levels
-        print('-----------------------------')
-        print(f'products_array: {products_array}')
-
         if products_array:
             products = list(dict.fromkeys(products_array))
             print(len(products))
 
+        # Sorting
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if sortkey == 'levels':
+                sortkey = 'level__name'
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+
+            products = products.order_by(sortkey)
+
+    current_sorting = f'{sort}_{direction}'
+
     context = {
         'products': products,
+        'current_sorting': current_sorting
     }
 
     return render(request, 'products/products.html', context)
