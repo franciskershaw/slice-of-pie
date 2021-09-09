bfdc0bf3a372dc2f4a8df6dab81e5b9d76799e3c
franciskershaw <franciskershaw2004@hotmail.com>
2021-09-09 14:07:43 +0000

    fix: duplicate products bug when filtering

--- products/views.py
+++ products/views.py
@@ -8,28 +8,52 @@ def all_products(request):
     """A view to return the all products page"""
 
     products = Product.objects.all()
+
     products_array = []
     products_materials = []
     products_angles = []
     products_levels = []
 
     if request.GET:
-        print(request.GET)
-        print(request.GET.getlist('material'))
+        print('-----------------------------')
+        print(f'Full get request: {request.GET}')
+        print('-----------------------------')
         if 'material' in request.GET:
+            print(f'Material GET request: {request.GET.getlist("material")}')
             materials = request.GET.getlist('material')
+            print(f'Materials: {materials}')
+            print('-----------------------------')
             products_materials = list(products.filter(material__name__in=materials))
+            print('-----------------------------')
+            print(f'products_materials: {products_materials}')
+            print('-----------------------------')
         if 'angle' in request.GET:
+            print(f'Angle GET request: {request.GET.getlist("angle")}')
             angles = request.GET.getlist('angle')
-            products_angles = list(products.filter(material__name__in=angles))
+            print(f'Angles: {angles}')
+            print('-----------------------------')
+            products_angles = list(products.filter(angle__name__in=angles))
+            print(list(products.filter(angle__name__in=angles)))
+            print('-----------------------------')
+            print(f'products_angles: {products_angles}')
+            print('-----------------------------')
         if 'levels' in request.GET:
+            print(f'Levels GET request: {request.GET.getlist("levels")}')
             levels = request.GET.getlist('levels')
-            products_levels = list(products.filter(material__name__in=levels))
+            print(f'Levels: {levels}')
+            print('-----------------------------')
+            products_levels = list(products.filter(level__name__in=levels))
+            print('-----------------------------')
+            print(f'products_levels: {products_levels}')
+            print('-----------------------------')
+
         products_array = products_materials + products_angles + products_levels
-        print(products_array)
+        print('-----------------------------')
+        print(f'products_array: {products_array}')
 
         if products_array:
-            products = products_array
+            products = list(dict.fromkeys(products_array))
+            print(len(products))
 
     context = {
         'products': products,
