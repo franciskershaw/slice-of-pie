a486bfd7a9ec822b0888974fd7a043092a9bc5f9
franciskershaw <franciskershaw2004@hotmail.com>
2021-09-16 20:10:51 +0000

    start add to basket functionality

--- slice_of_pie/views.py
+++ slice_of_pie/views.py
@@ -1,10 +1,18 @@
-from django.shortcuts import redirect, HttpResponse, get_object_or_404
-from django.contrib import messages
-
-from products.models import Product
+from django.shortcuts import redirect
 
 
 def add_to_basket(request, item_id):
-    """ Add a quantity of the specified product to the shopping bag """
+    """ Add a quantity of the specified product to the shopping basket """
 
-    print('Add to bag function has been called')
+    quantity = int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url')
+    basket = request.session.get('basket', {})
+
+    if item_id in list(basket.keys()):
+        basket[item_id] += quantity
+    else:
+        basket[item_id] = quantity
+
+    request.session['basket'] = basket
+    print(request.session['basket'])
+    return redirect(redirect_url)
