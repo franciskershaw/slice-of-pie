a53faf4b1ab9a9bf2e72765f0a4e9e971fc84849
franciskershaw <franciskershaw2004@hotmail.com>
2021-10-05 21:01:36 +0000

    add link to checkout in basket sidebar

--- checkout/views.py
+++ checkout/views.py
@@ -8,6 +8,7 @@ def checkout(request):
     bag = request.session.get('bag', {})
     if not bag:
         messages.error(request, "There's nothing in your bag yet")
+        print('BOO')
         return redirect(reverse('products'))
 
     order_form = OrderForm()
