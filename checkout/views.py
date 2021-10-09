81484d8c9671983d8b99e48bc6e1f79b7e30dd64
franciskershaw <franciskershaw2004@hotmail.com>
2021-10-09 16:37:44 +0000

    add stripe js and css to checkout

--- checkout/views.py
+++ checkout/views.py
@@ -15,6 +15,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51J9onGJgDRRMupsm5uG2rYlRfvC1JFA8wGaa7DolbBPFAc8PS9UU3ZtuPaC6rbWIwi5vRkj6ybJOrDfqrRjRXfZ100fcIV6unS',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
