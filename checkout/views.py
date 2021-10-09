61c45f271bc591e5441ee0ca6f9409131076877d
franciskershaw <franciskershaw2004@hotmail.com>
2021-10-09 20:23:40 +0000

    stripe: create first payment

--- checkout/views.py
+++ checkout/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
+from django.conf import settings
 
 from .forms import OrderForm
 from slice_of_pie.contexts import basket_contents
 
+import stripe
+
 
 def checkout(request):
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+
     basket = request.session.get('basket', {})
     if not basket:
         messages.error(request, "There's nothing in your basket yet")
@@ -14,13 +20,22 @@ def checkout(request):
     current_basket = basket_contents(request)
     total = current_basket['grand_total']
     stripe_total = round(total * 100)
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY,
+    )
 
     order_form = OrderForm()
+
+    if not stripe_public_key:
+        messages.warning(request, 'Stripe key is missing. Check your environment variables.')
+
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
-        'stripe_public_key': 'pk_test_51J9onGJgDRRMupsm5uG2rYlRfvC1JFA8wGaa7DolbBPFAc8PS9UU3ZtuPaC6rbWIwi5vRkj6ybJOrDfqrRjRXfZ100fcIV6unS',
-        'client_secret': 'test client secret',
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
     }
 
     return render(request, template, context)
--- slice_of_pie/settings.py
+++ slice_of_pie/settings.py
@@ -205,6 +205,9 @@ if 'USE_AWS' in os.environ:
 # Stripe payments
 FREE_DELIVERY_THRESHOLD = 0
 STANDARD_DELIVERY_PERCENTAGE = 5
+STRIPE_CURRENCY = 'usd'
+STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
+STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
