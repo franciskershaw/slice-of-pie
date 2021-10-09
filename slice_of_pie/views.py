d1425532940780235fa77d951da51ca464f7c672
franciskershaw <franciskershaw2004@hotmail.com>
2021-10-09 15:52:16 +0000

    finish toast functionality

--- slice_of_pie/views.py
+++ slice_of_pie/views.py
@@ -14,6 +14,7 @@ def add_to_basket(request, item_id):
 
     if item_id in list(basket.keys()):
         basket[item_id] += quantity
+        messages.success(request, f'Updated {product.name} quantity to {basket[item_id]}')
     else:
         basket[item_id] = quantity
         messages.success(request, f'Added {product.name} to your basket')
@@ -26,14 +27,17 @@ def add_to_basket(request, item_id):
 def adjust_basket(request, item_id):
     """ Adjust the quantity of an item in the basket """
 
+    product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     basket = request.session.get('basket', {})
 
     if quantity > 0:
         basket[item_id] = quantity
+        messages.success(request, f'Updated {product.name} quantity to {basket[item_id]}')
     else:
         basket.pop(item_id)
+        messages.success(request, f'Removed {product.name} from your basket')
 
     request.session['basket'] = basket
     return redirect(redirect_url)
@@ -41,13 +45,15 @@ def adjust_basket(request, item_id):
 
 def remove_item(request, item_id):
     """ Remove item entirely from basket """
-
+    product = get_object_or_404(Product, pk=item_id)
     try:
         basket = request.session.get('basket', {})
         basket.pop(item_id)
+        messages.success(request, f'Removed {product.name} from your basket')
 
         request.session['basket'] = basket
         return HttpResponse(status=200)
 
     except Exception as e:
+        messages.error(request, f'Error removing item: {e}')
         return HttpResponse(status=500)
