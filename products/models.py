50b52b14296a545a907de858c9950a7bed0f0274
franciskershaw <franciskershaw2004@hotmail.com>
2021-11-26 18:34:52 +0000

    amend products and material models

--- products/models.py
+++ products/models.py
@@ -5,6 +5,7 @@ class Material(models.Model):
 
     name = models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=254, null=False, blank=False)
+    is_sustainable = models.BooleanField(default=False)
 
     def __str__(self):
         return self.name
