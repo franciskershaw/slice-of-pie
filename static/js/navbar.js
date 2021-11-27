d5bb136c446068af160c2273642b33857e952ed8
franciskershaw <franciskershaw2004@hotmail.com>
2021-11-27 13:58:12 +0000

    use js to fix navbar id validation issue

--- static/js/navbar.js
+++ static/js/navbar.js
@@ -1,3 +1,22 @@
+/* Give the correct profile button the navBarDropdown ID */
+let mobileDropdown = document.querySelector('[data-screen="small"]');
+let largerDropdown = document.querySelector('[data-screen="large"]');
+
+if (window.innerWidth > 767) {
+  largerDropdown.setAttribute('id', 'navbarDropdown');
+} else {
+  mobileDropdown.setAttribute('id', 'navbarDropdown');
+}
+window.addEventListener('resize', () => {
+  if (window.innerWidth > 767) {
+      mobileDropdown.removeAttribute('id', 'navbarDropdown');
+      largerDropdown.setAttribute('id', 'navbarDropdown');
+  } else {
+      largerDropdown.removeAttribute('id', 'navbarDropdown')
+      mobileDropdown.setAttribute('id', 'navbarDropdown');
+  }
+})
+
 /* Source - responsive navigation bar sourced and tweaked to spec from https://www.aleksandrhovhannisyan.com/blog/responsive-navbar-without-bootstrap/ */
 const navbar = document.getElementById("navigation-bar");
 const navbarToggle = navbar.querySelector(".navbar-toggle");
