3f93fff2457749afa67f44780016aa9b8f7ada8d
franciskershaw <franciskershaw2004@hotmail.com>
2021-11-27 19:53:39 +0000

    beautify js files and scripts

--- static/js/navbar.js
+++ static/js/navbar.js
@@ -5,24 +5,24 @@ let mobileMenu = document.querySelector('[data-screen="menu-small');
 let largerMenu = document.querySelector('[data-screen="menu-large');
 
 if (window.innerWidth > 767) {
-  largerDropdown.setAttribute('id', 'navbarDropdown');
-  largerMenu.setAttribute('aria-labelledby', 'navbarDropdown');
+	largerDropdown.setAttribute('id', 'navbarDropdown');
+	largerMenu.setAttribute('aria-labelledby', 'navbarDropdown');
 } else {
-  mobileDropdown.setAttribute('id', 'navbarDropdown');
-  mobileMenu.setAttribute('aria-labelledby', 'navbarDropdown');
+	mobileDropdown.setAttribute('id', 'navbarDropdown');
+	mobileMenu.setAttribute('aria-labelledby', 'navbarDropdown');
 }
 window.addEventListener('resize', () => {
-  if (window.innerWidth > 767) {
-      mobileDropdown.removeAttribute('id', 'navbarDropdown');
-      mobileMenu.removeAttribute('aria-labelledby', 'navbarDropdown');
-      largerDropdown.setAttribute('id', 'navbarDropdown');
-      largerMenu.setAttribute('aria-labelledby', 'navbarDropdown');
-  } else {
-      largerDropdown.removeAttribute('id', 'navbarDropdown');
-      largerMenu.removeAttribute('aria-labelledby', 'navbarDropdown');
-      mobileDropdown.setAttribute('id', 'navbarDropdown');
-      mobileMenu.setAttribute('aria-labelledby', 'navbarDropdown');
-  }
+	if (window.innerWidth > 767) {
+		mobileDropdown.removeAttribute('id', 'navbarDropdown');
+		mobileMenu.removeAttribute('aria-labelledby', 'navbarDropdown');
+		largerDropdown.setAttribute('id', 'navbarDropdown');
+		largerMenu.setAttribute('aria-labelledby', 'navbarDropdown');
+	} else {
+		largerDropdown.removeAttribute('id', 'navbarDropdown');
+		largerMenu.removeAttribute('aria-labelledby', 'navbarDropdown');
+		mobileDropdown.setAttribute('id', 'navbarDropdown');
+		mobileMenu.setAttribute('aria-labelledby', 'navbarDropdown');
+	}
 });
 
 /* Source - responsive navigation bar sourced and tweaked to spec from https://www.aleksandrhovhannisyan.com/blog/responsive-navbar-without-bootstrap/ */
@@ -30,28 +30,28 @@ const navbar = document.getElementById("navigation-bar");
 const navbarToggle = navbar.querySelector(".navbar-toggle");
 
 function openMobileNavbar() {
-  navbar.classList.add("opened");
-  navbarToggle.setAttribute("aria-label", "Close navigation menu.");
+	navbar.classList.add("opened");
+	navbarToggle.setAttribute("aria-label", "Close navigation menu.");
 }
 
 function closeMobileNavbar() {
-  navbar.classList.remove("opened");
-  navbarToggle.setAttribute("aria-label", "Open navigation menu.");
+	navbar.classList.remove("opened");
+	navbarToggle.setAttribute("aria-label", "Open navigation menu.");
 }
 
 navbarToggle.addEventListener("click", () => {
-  if (navbar.classList.contains("opened")) {
-    closeMobileNavbar();
-  } else {
-    openMobileNavbar();
-  }
+	if (navbar.classList.contains("opened")) {
+		closeMobileNavbar();
+	} else {
+		openMobileNavbar();
+	}
 });
 
 const navbarMenu = navbar.querySelector(".navbar-menu");
 const navbarLinksContainer = navbar.querySelector(".navbar-links");
 
 navbarLinksContainer.addEventListener("click", (clickEvent) => {
-  clickEvent.stopPropagation();
+	clickEvent.stopPropagation();
 });
 
 navbarMenu.addEventListener("click", closeMobileNavbar);
